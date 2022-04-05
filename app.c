51ef8524d1e9e040a8a9e6f37a6f0aaf40318063
khcatino <kcatino@itba.edu.ar>
2022-04-05 19:13:30 -0300

    pruebas con pipes y envio de strings

--- app.c
+++ app.c
@@ -5,38 +5,72 @@
 
 #define PROCESSES 4
 
-typedef struct {
+typedef struct
+{
     char name[80];
     int number;
 } data;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     int pids[PROCESSES];
-    int writePipes[PROCESSES + 1][2];
-    int readPipes[PROCESSES + 1][2];
+    int writePipes[PROCESSES][2];
+    int readPipes[PROCESSES][2];
     int i;
-    for (i = 0; i < PROCESSES + 1; i++) {
-        if (pipe(writePipes[i]) == -1) { printf("Error with creating pipe\n"); return 1; }
-        if (pipe(readPipes[i]) == -1) { printf("Error with creating pipe\n"); return 1; }
+
+    for (i = 0; i < PROCESSES + 1; i++)
+    {
+        if (pipe(writePipes[i]) == -1)
+        {
+            printf("Error with creating pipe\n");
+            return 1;
+        }
+        if (pipe(readPipes[i]) == -1)
+        {
+            printf("Error with creating pipe\n");
+            return 1;
+        }
     }
 
-    for (i = 0; i < PROCESSES; i++) {
+    for (i = 0; i < PROCESSES; i++)
+    {
         pids[i] = fork();
-        if (pids[i] == -1) { printf("Error with creating process\n"); return 2; }
+        if (pids[i] == -1)
+        {
+            printf("Error with creating process\n");
+            return 2;
+        }
 
-        if (pids[i] == 0) {
+        if (pids[i] == 0)
+        {
             // Child
 
-        int readfd = writePipes[i][0];
-        int writefd = readPipes[i][1];
+        
+            int readfd = writePipes[i][0];
+            int writefd = readPipes[i][1];
 
-        dup2(readfd, STDIN);
+            int j;
+            for (j = 0; j< PROCESSES; j++) {
+                if (j != i) {
+                    close(writePipes[j][0]);
+                    close(writePipes[j][1]);
+                }
+            }
+            
 
-        char *args[]={"./child"};
+            dup2(readfd, STDIN_FILENO);
+            dup2(writefd, STDOUT_FILENO);
 
-        if(execvp(args[0],args) == -1) {
-            perror("Error al ejecutar programa hijo");
-        }
+            close(writefd);
+            close(readfd);
+
+            char *args[] = {"./child", NULL};
+
+            if (execvp(args[0], args) == -1)
+            {
+                perror("Error al ejecutar programa hijo");
+            }
+
 
         }
     }
@@ -44,25 +78,47 @@ int main(int argc, char* argv[]) {
     // Parent
 
     int j;
-    for (j = 0; j < PROCESSES + 1; j++) {
-        if (j != PROCESSES) {
-            close(writePipes[j][0]);
-            close(readPipes[j][1]);
+    for (j = 0; j < PROCESSES; j++)
+    {
+        close(writePipes[j][0]);
+        close(readPipes[j][1]);
+    }
+
+    // while(1) {
+    // // select()
+    //     // si p1 esta ready:
+	// // read(p1)
+	// // postear en shm
+	// // si hay mas tareas:
+	// 	// write(p2)
+	// // else:
+	// 	// close(p2)
+
+        
+    // }
+    for (i = 0 ; i < PROCESSES ; i++) {
+        char * s = "Holaaaa\n";
+        int len = strlen(s);
+        if (write(writePipes[i][1], &len, sizeof(int)) == -1) {
+            perror("Error at writing\n");
+            exit(2);
+        }
+        if (write(writePipes[i][1], s, sizeof(char)*(len+1)) == -1) {
+            perror("Error at writing\n");
+            exit(2);
         }
     }
 
-    for (i = 0; i < PROCESSES; i++) {
-        int a = i;
-        if (write(writePipes[i][1],&a, sizeof(int)) == -1) { printf("Error at writing\n"); return 4; }
+    for (i = 0 ; i < PROCESSES ; i++) {
+        int length;
+        char s[100] = {0};
+        read(readPipes[i][0], &length, sizeof(int));
+        read(readPipes[i][0], s, sizeof(char)*(length + 1));
+        printf(s);
     }
 
-    for (i = 0; i < PROCESSES; i++) {
-        int a;
-        if (read(readPipes[i][0], &a, sizeof(int)) == -1) { printf("Error at reading\n"); return 3; }
-        printf("Recibi %d en main\n", a);
-    }
-
-    for (i = 0; i < PROCESSES; i++) {
+    for (i = 0; i < PROCESSES; i++)
+    {
         wait(NULL);
     }
     return 0;
--- child.c
+++ child.c
@@ -1,17 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+char path[100] = {0};
+int length;
+
 int main(int argc, char * argv[]) {
 
-    int readfd = writePipes[i][0];
-    int writefd = readPipes[i][1];
-    int y;
-    if (read(readfd, &y, sizeof(int)) == -1) { printf("Error at reading\n"); return 3; }
-    printf("Procese archivo y recibi %d\n", y);
-    printf("Envio valor %d a main\n", y);
-    if (write(writefd,&y, sizeof(int)) == -1) { printf("Error at writing\n"); return 4; }
+    // if (read(STDIN_FILENO, &y, sizeof(int)) == -1) { printf("Error at reading\n"); return 3; }
+    // if (write(STDOUT_FILENO,&y, sizeof(int)) == -1) { printf("Error at writing\n"); return 4; }
+
+        // Del lado del worker
+    int r;
+    if((r = read(STDIN_FILENO, &length, sizeof(int)) == -1)) { perror("Error at reading\n"); return 5; }
+
+    if ((r = read(STDIN_FILENO, path, sizeof(char)*(length + 1))) == -1) { perror("Error at reading\n"); return 5; }
+
+    if ((r = write(STDOUT_FILENO, &length, sizeof(int))) == -1) { perror("Error at writing\n"); return 5; }
+
+    if ((r = write(STDOUT_FILENO, path, sizeof(char)*(length + 1))) == -1) { perror("Error at writing\n"); return 5; }
+
+    printf(path);
 
 
-    // char *args[]={"/home/kevincatino/Arqui/tp0/p",NULL};
-    // if(execvp(args[0],args) == -1) {
-    //     perror("Error al ejecutar programa padre");
+    // if (r == 0) {
+    //     // no hay mas trabajo
+    //     close(STDIN_FILENO);
+    //     exit(0);
     // }
+
+    // else { perror("read"); exit(2); }
+        
+
     return 0;
 }
